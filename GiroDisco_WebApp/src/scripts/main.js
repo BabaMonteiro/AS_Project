cb32f118cc7da13c56462241891e93d6db1bc46b
hugoc1944 <125464948+hugoc1944@users.noreply.github.com>
2023-06-02 17:53:50 +0100

    Página Home e Adição de user popup em todas as páginas

--- GiroDisco_WebApp/src/scripts/main.js
+++ GiroDisco_WebApp/src/scripts/main.js
@@ -1,17 +1,7 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let toggle = document.querySelector('.toggle');
-let toggleBtn = document.querySelector('.btn-start');
-let showContent = document.querySelector('.oflw');
-let content = document.querySelector('.cnt-1');
-menu.onclick = () => {
-    navbar.classList.toggle('open');
-}
-toggleBtn.onclick = () => {
-    toggle.style.opacity = "0";
-    showContent.style.overflowY = "scroll";
-    setTimeout(function() {toggle.style.display = "none";     content.style.opacity = "100";}, 550);
-}
+
+
 //Navbar scroll
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
