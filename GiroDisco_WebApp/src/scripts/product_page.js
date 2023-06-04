da3e2c52261f6dbc7eac7d14156cf185a89bc0ca
Luis Leal <lecl@ua.pt>
2023-06-04 21:52:01 +0100

    Album product pages created

---
+++ GiroDisco_WebApp/src/scripts/product_page.js
@@ -0,0 +1,12 @@
+const thumbnails = document.querySelectorAll(".photo-thumbnails img");
+const mainPhoto = document.getElementById("main-photo");
+
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", () => {
+    const thumbnailSrc = thumbnail.getAttribute("src");
+    const mainPhotoSrc = mainPhoto.getAttribute("src");
+
+    thumbnail.setAttribute("src", mainPhotoSrc);
+    mainPhoto.setAttribute("src", thumbnailSrc);
+  });
+});
