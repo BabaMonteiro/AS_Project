1eedecf8592a22279c60cbcfc5cd68de34ff71e6
BabaMonteiro <barbarammoreira@ua.pt>
2023-06-05 02:03:21 +0100

    markers

--- GiroDisco_WebApp/src/scripts/map.js
+++ GiroDisco_WebApp/src/scripts/map.js
@@ -1,22 +1,40 @@
-var map = L.map('map').setView([40.64095818867095, -8.653921595656492], 17);
+var map = L.map('map').setView([40.64095818867095, -8.653921595656492], 15);
 
-        var myIcon = L.icon({
-            iconUrl: '../../assets/img/logo.png', // Replace with the path to your vinyl store icon image
-            iconSize: [30, 30],
-            iconAnchor: [15, 35],
-            popupAnchor: [0, -30]
-        });
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+var geojson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -8.65230552671622,
+          40.63815571249049
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          -8.654572328836025,
+          40.64210328193036
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
 
-        $.getJSON("vinil_stores.geojson", function (data) {
-            var vinylStores = L.geoJSON(data.features, {
-                pointToLayer: function (feature, latlng) {
-                    return L.marker(latlng, {
-                        icon: myIcon
-                    });
-                },
-            }).addTo(map);
-        });
-
-        $(".leaflet-control-attribution").remove();
+L.geoJSON(geojson, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng);
+  },
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup('<h3>' + feature.properties.name + '</h3>');
+  }
+}).addTo(map);
